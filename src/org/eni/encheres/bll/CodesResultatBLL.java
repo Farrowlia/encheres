210186559fab97a28a8610722a93cc0336e4ba63
Equilibrium2046 <79090637+Equilibrium2046@users.noreply.github.com>
2021-02-17 14:43:00 +0100

    Bo, dal et bll Categorie et Enchere

--- src/org/eni/encheres/bll/CodesResultatBLL.java
+++ src/org/eni/encheres/bll/CodesResultatBLL.java
@@ -7,7 +7,14 @@ public abstract class CodesResultatBLL {
 	public static final int REGLE_CODEPOSTAL_RETRAIT_ERREUR = 20002;
 	public static final int REGLE_VILLE_RETRAIT_ERREUR = 20003;
 	
+	// Code erreur concernant CATEGORIE
+	public static final int REGLE_LIBELLE_ERREUR = 20100;
+	
+	// Code erreur concernant ENCHERE
+	public static final int REGLE_DATE_ENCHERE_ERREUR = 20200;
+	public static final int REGLE_MONTANT_ENCHERE_ERREUR = 20201;
+	
 	// Code erreur concernant ?
-	public static final int REGLE_XXXXXX_ERREUR = 20100;
+	public static final int REGLE_XXXXXX_ERREUR = 20300;
 	
 }
